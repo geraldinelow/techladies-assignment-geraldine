d7bb0c3a2b419110458eeacb0f89134352041693
Geraldine <geraldinelhy@gmail.com>
2019-09-22 22:01:24 +0800

    Updates

---
+++ precache-manifest.c696bd66c1114dbb1b7692c512dd0d96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21c8747f017a4bf54209764851b80c5",
+    "url": "/techladies/index.html"
+  },
+  {
+    "revision": "e0b20a24c4514142992b",
+    "url": "/techladies/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "af1d961152c0e8aba2a7",
+    "url": "/techladies/static/css/main.bb5477f2.chunk.css"
+  },
+  {
+    "revision": "e0b20a24c4514142992b",
+    "url": "/techladies/static/js/2.5bc2ff44.chunk.js"
+  },
+  {
+    "revision": "af1d961152c0e8aba2a7",
+    "url": "/techladies/static/js/main.3e91acd3.chunk.js"
+  },
+  {
+    "revision": "65f2f84230f262ce0abf",
+    "url": "/techladies/static/js/runtime~main.6f4966e4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techladies/static/media/logo.5d5d9eef.svg"
+  }
+]);
